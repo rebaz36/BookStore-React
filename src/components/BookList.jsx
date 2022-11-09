cdc09c15c9f8c12466dd5f723006379de307aea1
rebaz36 <rebaz415@gmail.com>
2022-11-09 09:29:56 +0300

    Add books to Booklist

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBooks } from '../redux/books/books';
 import Book from './Book';
 
 function Booklist() {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  });
   return (
     <ul>
       {books.map((book) => (
-        <li key={book.title}>
-          <Book title={book.title} author={book.author} id={book.id} />
+        <li key={book.item_id}>
+          <Book
+            title={book.title}
+            author={book.author}
+            item_id={book.item_id}
+            category={book.category}
+          />
         </li>
       ))}
     </ul>
