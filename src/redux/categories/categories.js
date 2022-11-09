a8dc1f1aaa69bcc422e2958bb6f2fe4ffe428d26
rebaz36 <rebaz415@gmail.com>
2022-11-09 11:18:23 +0300

    update category reducer

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,18 +1,37 @@
 // actions
-const CHECK = '/src/redux/Categories/CHECK';
+const GET_CATEGORIES = '/src/redux/Categories/GET_CATEGORIES';
 
 // reducer
 export default function categoriesReducer(state = [], action) {
   switch (action.type) {
-    case CHECK:
+    case GET_CATEGORIES:
       return action.payload;
     default:
       return state;
   }
 }
 
+// key = kcM4ut7Kvp9QgSxmujh0
+const API_KEY = 'kcM4ut7Kvp9QgSxmujh0';
+
 // action creators
-export const checkStatus = () => ({
-  type: CHECK,
-  payload: 'Work in Progress',
-});
+export const getCategories = () => async (dispatch) => {
+  const categoryFetch = await fetch(
+    `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${API_KEY}/books`,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    },
+  ).then((data) => data.json());
+  const category = [];
+  Object.keys(categoryFetch).forEach((e) => {
+    category.push({ ...categoryFetch[e][0], item_id: e });
+  });
+
+  dispatch({
+    type: GET_CATEGORIES,
+    payload: category,
+  });
+};
