83c3bb4d3b86cf6ba1e3f473bd3add22b99733e2
rebaz36 <rebaz415@gmail.com>
2022-11-10 01:36:02 +0300

    Update Redux Configuration

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,14 @@
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const store = configureStore(
-  {
-    reducer: {
-      books: booksReducer,
-      category: categoriesReducer,
-    },
-  },
-  applyMiddleware(thunk),
-);
+const bookReducers = combineReducers({
+  books: booksReducer,
+  category: categoriesReducer,
+});
+
+const store = configureStore({
+  reducer: bookReducers,
+});
 
 export default store;
