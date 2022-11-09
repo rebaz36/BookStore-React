7b25d1a8f863f56ec63c727c1b7029f4392a9ff9
rebaz36 <rebaz415@gmail.com>
2022-11-10 01:41:30 +0300

    Update AddBook Function import

--- src/components/AddBookForm.jsx
+++ src/components/AddBookForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/books';
+import { addBook } from '../redux/books/BooksAPI';
 
 function AddBookForm() {
   const dispatch = useDispatch();
@@ -11,6 +11,7 @@ function AddBookForm() {
 
   function postBook(e) {
     e.preventDefault();
+
     dispatch(
       addBook({
         title,
