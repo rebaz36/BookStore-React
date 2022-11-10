e7616d9be73222341de28e437ffbc6d62f1811ef
rebaz36 <rebaz415@gmail.com>
2022-11-10 14:06:30 +0300

    update root CSS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Header />
       <Routes>
         <Route path="/" element={<BooksView />} />
